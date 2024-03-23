c1709a6e6458a8b698c3ad79449b453cad048d32
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-23 18:20:59 +0100

    Added getter.

--- src/main/java/pl/edu/s28201/tpo_04/model/Publisher.java
+++ src/main/java/pl/edu/s28201/tpo_04/model/Publisher.java
@@ -23,4 +23,12 @@ public class Publisher {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
