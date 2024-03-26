6a7310617b335ea47421a90d1cd3bce486df8e21
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-26 08:08:42 +0100

    Added print of publishers and authors.

--- src/main/java/pl/edu/s28201/tpo_04/Tpo04Application.java
+++ src/main/java/pl/edu/s28201/tpo_04/Tpo04Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pl.edu.s28201.tpo_04.repository.AuthorRepository;
 import pl.edu.s28201.tpo_04.repository.BookRepository;
+import pl.edu.s28201.tpo_04.repository.PublisherRepository;
 
 @SpringBootApplication
 public class Tpo04Application {
@@ -15,9 +17,13 @@ public class Tpo04Application {
     }
 
     @Bean
-    public CommandLineRunner run(BookRepository bookRepo) {
+    public CommandLineRunner run(BookRepository bookRepo,
+                                 PublisherRepository pubRepo,
+                                 AuthorRepository authRepo) {
         return args -> {
             bookRepo.findAll().forEach(System.out::println);
+            pubRepo.findAll().forEach(System.out::println);
+            authRepo.findAll().forEach(System.out::println);
         };
     }
 
